607d9a5b47da93ae590e59478b329df059fab943
José Carlos López <jcl.isc@gmail.com>
2017-05-04 12:50:01 -0500

    working realm

--- app/src/main/java/me/apexjcl/retrofitsample/activities/RealmShowcaseActivity.java
+++ app/src/main/java/me/apexjcl/retrofitsample/activities/RealmShowcaseActivity.java
@@ -2,14 +2,20 @@ package me.apexjcl.retrofitsample.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 
+import io.realm.Realm;
 import me.apexjcl.retrofitsample.R;
+import me.apexjcl.retrofitsample.adapters.RealmPostAdapter;
+import me.apexjcl.retrofitsample.models.Post;
 
 public class RealmShowcaseActivity extends AppCompatActivity {
 
+
+    private Realm realm;
 
     private EditText mTitle;
     private RecyclerView mRecyclerView;
@@ -20,9 +26,38 @@ public class RealmShowcaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_realm_showcase);
         mTitle = (EditText) findViewById(R.id.title);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        realm = Realm.getDefaultInstance(); // cada que la aplicación inicie, obtendremos el realm
+        mRecyclerView.setAdapter(new RealmPostAdapter(
+                getApplicationContext(),
+                realm.where(Post.class).findAllAsync()
+        ));
     }
 
     public void addPost(View v) {
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm bgRealm) {
+                Post p = bgRealm.createObject(Post.class);
+                p.setTitle(mTitle.getText().toString());
+                p.setBody("Nuevo");
+                p.setId(1);
+                bgRealm.copyToRealm(p);
+            }
+        });
+    }
 
+
+    @Override
+    protected void onStop() {
+        if (realm != null)
+            realm.close();
+        realm = null;
+        super.onStop();
     }
 }
--- app/src/main/java/me/apexjcl/retrofitsample/adapters/RealmPostAdapter.java
+++ app/src/main/java/me/apexjcl/retrofitsample/adapters/RealmPostAdapter.java
@@ -1,8 +1,56 @@
 package me.apexjcl.retrofitsample.adapters;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
+import me.apexjcl.retrofitsample.R;
+import me.apexjcl.retrofitsample.models.Post;
+
 /**
  * Created by apex on 04/05/2017.
  */
 
-public class RealmPostAdapter {
+public class RealmPostAdapter extends RealmRecyclerViewAdapter<Post, RealmPostAdapter.ViewHolder> {
+
+    public RealmPostAdapter(@NonNull Context context, @Nullable OrderedRealmCollection<Post> data) {
+        super(context, data, true);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.update(position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTitle;
+        private TextView mBody;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTitle = (TextView) itemView.findViewById(R.id.title);
+            mBody = (TextView) itemView.findViewById(R.id.body);
+        }
+
+
+        public void update(int position) {
+            Post mPost = getItem(position);
+            mTitle.setText(mPost.getTitle());
+            mBody.setText(mPost.getBody());
+        }
+    }
+
 }
